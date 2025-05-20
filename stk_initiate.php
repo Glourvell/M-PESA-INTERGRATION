eaa8fa4fa21a910fc7189e475a6f06a815667489
Glourvell <149944607+Glourvell@users.noreply.github.com>
2025-05-19 23:57:41 -0700

    Update stk_initiate.php

--- stk_initiate.php
+++ stk_initiate.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])){
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
-  $BusinessShortCode = '174379';
+  $BusinessShortCode = '222111';
   $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
   
   /*
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
   */
   
    $PartyA = $_POST['phone']; // This is your phone number, 
-  $AccountReference = '2255';
+  $AccountReference = '9148945';
   $TransactionDesc = 'Test Payment';
   $Amount = $_POST['amount'];
  
